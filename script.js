3529eefb92228a789d2e3e941195fc79b9b25e06
adelademarku <adelademarku@email.com>
2022-11-08 15:34:07 +0100

    21

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let nome = prompt("Inserisci il nome");
 let cognome = prompt("Inserisci il cognome");
 let colorePreferito = prompt("Inserisci il colore preferito");
-
+let numero = prompt ("Inserisci 21")
 
 
 /* Console log */
@@ -13,6 +13,7 @@ console.log(
         Nome: ${nome}
         Cognome: ${cognome}
         Colore Preferito: ${colorePreferito}
+        21              :  ${numero}
     `
 );
 
