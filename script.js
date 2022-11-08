efc7d2087dd3e7545b2cf865b0fca2e621a3dbc4
adelademarku <adelademarku@email.com>
2022-11-08 15:26:37 +0100

    inserito variabile e console

---
+++ script.js
@@ -0,0 +1,18 @@
+//creazione variabile 
+
+let nome = prompt("Inserisci il nome");
+let cognome = prompt("Inserisci il cognome");
+let colorePreferito = prompt("Inserisci il colore preferito");
+
+
+
+/* Console log */
+
+console.log(
+    `
+        Nome: ${nome}
+        Cognome: ${cognome}
+        Colore Preferito: ${colorePreferito}
+    `
+);
+
