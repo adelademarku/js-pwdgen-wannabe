086f3bb01755b61c8d6196fcc3f0640deb1e0ba8
adelademarku <adelademarku@email.com>
2022-11-08 15:54:04 +0100

    aggiunto divisione

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ console.log(
     `
 );
 
+
+let numero1 = prompt("Inserisci un numero")
+let numero2 = prompt ("Inserisci un altro numero")
+
+numero1 = parseInt(numero1);
+console.log("Quanto vale numero1: " + numero1)
+
+numero2 = parseInt(numero2);
+console.log("Quanto vale numero2: " + numero2)
+
+let divisione = numero1 / numero2;
+
+
+
+console.log( "Risultato divisione: " + divisione);
+
